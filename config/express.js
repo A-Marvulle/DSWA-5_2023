3cc1fc7389fa8da93300bfcaad80639471021f0a
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2021-12-29 23:04:16 -0300

    Alterado o parâmetro da porta e versão do node.

--- config/express.js
+++ config/express.js
@@ -2,26 +2,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var load = require('express-load');
 
-module.exports = function(){
-	//Instância do Express
-	var app = express();
+module.exports = function() {
+    //Instância do Express
+    var app = express();
 
-	//Porta da aplicação	
-	//app.set('port', 3000);
-	app.set('port', process.env.PORT);
+    //Porta da aplicação	
+    //app.set('port', 3000);
+    app.set('port', process.env.PORT || 3000);
 
-	//Middleware
-	app.use(express.static('./public'));
-	app.use(bodyParser.urlencoded({extended: true}));
-	app.use(bodyParser.json());
-	app.use(require('method-override')());
+    //Middleware
+    app.use(express.static('./public'));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
 
     //Definir Engine para a View
-	app.set('view engine', 'ejs');
-	app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
 
     //Carregar pastas
-	load('models', {cwd: 'app'}).then('controllers').then('routes').into(app);
+    load('models', { cwd: 'app' }).then('controllers').then('routes').into(app);
 
-	return app;
+    return app;
 };
