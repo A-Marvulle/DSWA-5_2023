b6d9113251003ae4be77c842e38e3ec6968c6413
A-Marvulle <107883417+A-Marvulle@users.noreply.github.com>
2023-05-12 22:12:30 +0000

    .
    .

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb+srv://alfredomarvulle:1234@cluster1.lotw2pl.mongodb.net/dswa?retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
