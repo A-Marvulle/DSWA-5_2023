4576e852f596a2b285776d4d71ae529bebc91238
A-Marvulle <107883417+A-Marvulle@users.noreply.github.com>
2023-05-15 15:48:15 +0000

    alterações tela de cadastro

--- app/models/curso.js
+++ app/models/curso.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 module.exports = function() {
     var schema = mongoose.Schema({
-        nome: {type: String, required: true},
+        curso: {type: String, required: true},
         coordenador: { type: String, required: true, index: { unique: true } }
     });
      return mongoose.model('Curso', schema);
