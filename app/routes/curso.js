5fd56b9cb858527768318d469ecf03e308f6e9e1
A-Marvulle <107883417+A-Marvulle@users.noreply.github.com>
2023-05-26 22:13:28 +0000

    curso

--- app/routes/curso.js
+++ app/routes/curso.js
@@ -1,9 +1,17 @@
+function verificaAutenticacao(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.status('401').json('Não autorizado');
+    }
+}
+
 module.exports = function(app) {
     var controller = app.controllers.curso;
     app.route('/cursos')
-        .get(controller.listaCursos)
-        .post(controller.salvaCurso);
+        .get(verificaAutenticacao, controller.listaCursos)
+        .post(verificaAutenticacao, controller.salvaCurso);
     app.route('/cursos/:id')
-        .get(controller.obtemCurso)
-        .delete(controller.removeCurso);
+        .get(verificaAutenticacao, controller.obtemCurso)
+        .delete(verificaAutenticacao, controller.removeCurso);
 };
